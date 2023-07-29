132af0087c086b4ee339c9849da4eb15b9529ee8
Greg You <dryudryu@gmail.com>
2023-07-29 19:52:09 +0900

    Add files

---
+++ diabloData.js
@@ -0,0 +1,31 @@
+
+var diablo4List = [{"logo":"H","type":"diablo4","title":"Helltides.com","url":"https://helltides.com/"},
+{"logo":"M","type":"diablo4","title":"Diablo4 map","url":"https://diablo4.tactiks.info"},
+{"logo":"P","type":"diablo4","title":"Diablo4 profile","url":"https://d4armory.io"},
+{"logo":"I","type":"diablo4","title":"Diablo4 Inven","url":"https://m.inven.co.kr/diablo4/"},
+{"logo":"I","type":"diablo4","title":"Inven<br>(Freeboard)","url":"https://m.inven.co.kr/board/diablo4/6025"},
+{"logo":"I","type":"diablo4","title":"Inven<br>(Babarian)","url":"https://www.inven.co.kr/board/diablo4/6029"},
+{"logo":"I","type":"diablo4","title":"Inven<br>(Druid)","url":"https://www.inven.co.kr/board/diablo4/6030"},
+{"logo":"I","type":"diablo4","title":"Inven<br>(Necromancer)","url":"https://m.inven.co.kr/board/diablo4/6031"},
+{"logo":"I","type":"diablo4","title":"Inven<br>(Rogue)","url":"https://m.inven.co.kr/board/diablo4/6032"},
+{"logo":"I","type":"diablo4","title":"Inven<br>(Mage)","url":"https://m.inven.co.kr/board/diablo4/6033"},
+{"logo":"I","type":"diablo4","title":"Inven<br>(Skilltree)","url":""},
+{"logo":"O","type":"diablo4","title":"Diablo4 OST","url":"https://www.youtube.com/watch?v=uRqjKZfLU7M&list=PLsQpV9OLWf1L7xNT1ApnCmxGfqCsbP4fT&index=1"},
+{"logo":"T","type":"diablo4","title":"Diablo4<br>(Trailer)","url":"https://www.youtube.com/watch?v=M_gXds_w9xs"},
+{"logo":"R","type":"diablo4","title":"Ruliweb<br>(Board)","url":"https://m.ruliweb.com/family/2086/board/185248"},
+{"logo":"R","type":"diablo4","title":"Ruliweb<br>(Wiki)","url":"https://m.ruliweb.com/game/85248/read/61?"}];
+
+var diablo3List = [{"logo":"O","type":"diablo3","title":"Diablo3 OST","url":"https://www.youtube.com/watch?v=XhrKNPHGR8E&list=PLBF5FDBFF7A2EB549"},
+{"logo":"T","type":"diablo3","title":"Diablo3<br>(Trailer)","url":"https://www.youtube.com/watch?v=M_c1q2oMHlk"},
+{"logo":"P","type":"diablo3","title":"Diablo3 profile","url":"https://vue-diablo3-app.netlify.app/profile-finder"},
+{"logo":"I","type":"diablo3","title":"Diablo3 Inven","url":"https://m.inven.co.kr/diablo3/"}];
+
+var diablo2List = [{"logo":"O","type":"diablo2","title":"Diablo2 OST","url":"https://youtube.com/playlist?list=PLD80CC3FEBE323161&feature=sharec"},
+{"logo":"T","type":"diablo2","title":"Diablo2<br>(Trailer)","url":"https://www.youtube.com/watch?v=UFVIjMSsB70&list=PLBZDjdoBMq20y1NuftFeezY07VIUUYmjj"},
+{"logo":"D","type":"diablo2","title":"Diablo2.io","url":"https://diablo2.io/index.php"},
+{"logo":"I","type":"diablo2","title":"Diablo2 Inven","url":"https://m.inven.co.kr/diablo2/"}];
+
+var diablo1List = [{"logo":"O","type":"diablo1","title":"Diablo1 OST","url":"https://www.youtube.com/playlist?list=PL4C4D2047A5B1423D"},
+{"logo":"T","type":"diablo1","title":"Diablo1<br>(Trailer)","url":"https://www.youtube.com/watch?v=fnoSaoHlsEM"},
+{"logo":"W","type":"diablo1","title":"Diablo1<br>(Webgame)","url":"https://d07riv.github.io/diabloweb"},
+{"logo":"A","type":"diablo1","title":"Diablo1<br>(Android)","url":"https://play.google.com/store/apps/details?id=org.diasurgical.devilutionx&hl=en_US"}];
